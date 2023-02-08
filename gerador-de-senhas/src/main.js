7d889ce5d1609f24a7decb829b0dd0d914f1cdd3
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-02-08 16:09:03 -0300

    finalizando o gerador de senhas

--- gerador-de-senhas/src/main.js
+++ gerador-de-senhas/src/main.js
@@ -1,3 +1,10 @@
-import { nanoid } from "nanoid";
+import { nanoid, random } from "nanoid";
+import './style.css'
 
-console.log(nanoid())
+const passwordBrnEl = document.querySelector("button")
+const displayPasswordElement = document.querySelector('h2')
+
+passwordBrnEl.addEventListener('click', () => {
+    const randomPassword = nanoid();
+    displayPasswordElement.innerHTML = randomPassword;
+})
