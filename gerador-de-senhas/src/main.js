2ebce036ae6afb68051789256cb885806ca0c903
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-02-13 11:18:32 -0300

    comitando até aqui

--- gerador-de-senhas/src/main.js
+++ gerador-de-senhas/src/main.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import { nanoid } from 'nanoid';
+
+const btnSenhaSegura = document.querySelector('button');
+const h2 = document.querySelector('h2');
+
+btnSenhaSegura.addEventListener('click', () => {
+    h2.innerHTML = nanoid()
+})
